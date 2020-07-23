9f5b28e0183bba3a101c65775fd8a7f9e48d8ee2
Sam Ewald <ewalds2@uw.edu>
2020-07-23 10:05:12 -0700

    Introductory function, resetting global environment, setting and printing working directory.

--- indiana_counties.R
+++ indiana_counties.R
@@ -1,8 +1,15 @@
 # Indiana Counties
 
+# new_f = function(arg, arg_a){
+#   arg_b = 1
+#   return(arg + arg_b)
+# }
+
 # 1. Set up
 
-
+rm(list = ls())
+setwd("/ihme/homes/ewalds2/repos/indiana_counties")
+message(getwd())
 
 # 2. Read in data
 
